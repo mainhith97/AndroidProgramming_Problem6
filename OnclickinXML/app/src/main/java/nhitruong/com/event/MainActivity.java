3549c6a70751e6e9606d70d7749598227e3a19e2
Your Name <you@example.com>
2018-09-17 21:48:26 +0700

    first commit

---
+++ OnclickinXML/app/src/main/java/nhitruong/com/event/MainActivity.java
@@ -0,0 +1,32 @@
+package nhitruong.com.event;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEdtA, mEdtB;
+    private TextView mTvResult;
+    private Button mBtntong2so;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEdtA = (EditText) findViewById(R.id.edt_a);
+        mEdtB = (EditText) findViewById(R.id.edt_b);
+        mTvResult = (TextView) findViewById(R.id.tv_result);
+        mBtntong2so = (Button) findViewById(R.id.btn_tong2so);
+    }
+
+    public void onClicktong2so(View v){
+        int a = Integer.parseInt(mEdtA.getText().toString());
+        int b = Integer.parseInt(mEdtB.getText().toString());
+        mTvResult.setText("" + (a+b));
+    }
+}
